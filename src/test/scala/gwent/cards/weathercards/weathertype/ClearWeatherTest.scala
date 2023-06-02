3eb7eed92c5b5fd76e08daece9b4dc4fd0a4eb91
Poofakiin <benja.feli@gmail.com>
2023-06-02 03:08:10 -0400

    TEST(test.gwent.cards.weathercards.weathertype): Add test cases for classes ClearWeather and Frost

--- src/test/scala/gwent/cards/weathercards/weathertype/ClearWeatherTest.scala
+++ src/test/scala/gwent/cards/weathercards/weathertype/ClearWeatherTest.scala
@@ -1,6 +1,24 @@
 package cl.uchile.dcc
 package gwent.cards.weathercards.weathertype
 
-class ClearWeatherTest {
+import munit.FunSuite
 
+class ClearWeatherTest extends FunSuite{
+    var clearWeather: IWeatherType = _
+    var clearWeather2: IWeatherType = _
+
+    override def beforeEach(context: BeforeEach): Unit = {
+        clearWeather = ClearWeather()
+        clearWeather2 = ClearWeather()
+    }
+
+    test("A ClearWeather should have a name and it should be Clear Weather") {
+        assertEquals(clearWeather.name, "Clear Weather")
+    }
+
+    test("Any Clear Weather should be equal to another and it should have the same hashcode"){
+        assertEquals(clearWeather, clearWeather2)
+        assertEquals(clearWeather2, clearWeather)
+        assertEquals(clearWeather.hashCode(),clearWeather2.hashCode())
+    }
 }
